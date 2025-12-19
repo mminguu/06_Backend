cc7868f467c00563274fc96edb79b0a64606461d
kang <mminguuu7@gmail.com>
2025-12-19 10:57:05 +0900

    .

--- js-jquery/js/main.js
+++ js-jquery/js/main.js
@@ -6,12 +6,31 @@ $(document).ready(
         console.log('jquery 연결 확인 완료');
         $("#btn").click(function(){
             $("#text").text("버튼이 클릭되었습니다.");
-        }
-    )
-    // 전체선택
-    $("#checkall").on("change",function(){
-        $('.chk').prop("checked", $(this).prop("checked")); //.prop('checked',this.checked)
-    })
+        })
+        // 전체선택
+        $("#checkall").on("change",function(){
+            $('.chk').prop('checked',this.checked);
+        });
+        // 개별체크로 전체 컨트롤
+        $('.chk').on('change',function(){
+            const total = $('.chk').length
+            const checked = $('.chk:checked').length
+            $('#checkall').prop('checked',total==checked)
+            // 개수를 카운트
+            $('#count').text(checked)
+        });
+        // 선택 삭제
+        $('#deleteBtn').click(function(){
+            $('.tchk:checked').each(function(){
+                $(this).closest('tr').remove();
+            });
+        });
+        // 버튼 비활성화(중복 클릭 방지)  저장 결제 api 호출
+        $('#saveBtn').click(function(){
+            $(this).prop('disabled',true) // 버튼 활성화
+        setTimeout( ()=>{
+            $(this).prop('disabled',false) // 버튼 비활성화
+        },2000);
+        });
     }
-    
 );
