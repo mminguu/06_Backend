27cfefcd8ff1f4d2dd4db010cc0ac9da3e73d16f
kang <mminguuu7@gmail.com>
2025-12-24 09:27:32 +0900

    Django 실습

---
+++ Django/blog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render
+# Create your views here.
+def index(request):
+    question_list =  Question.objects.order_by('-create_at')
+    context = {'question_list' : question_list}
+    return render(request,'blog/question_list.html',context)
+
+def detail(request,question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question':question}
+    return render(request,'blog/question_detail.html',context)
+
+from django.utils import timezone
+from django.shortcuts import redirect
+def answer_create(request, question_id):  # url로 넘어온 데이터
+    question = Question.objects.get(id=question_id)
+    question.answer_set.create(content=request.POST.get('content'),create_at=timezone.now())
+    return redirect('blog:detail',question_id=question.id)
+    
